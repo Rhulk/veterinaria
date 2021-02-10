76b1590c514372984a6fa9a75f05ac31e6a76eb8
kk <quique1904@gmail.com>
2021-02-10 18:49:15 +0100

    add

--- js/mascotas.js
+++ js/mascotas.js
@@ -1,1 +1,37 @@
-console.log("Mascotas");
+
+const listaMascotas = document.getElementById('lista-mascotas');
+const tipo = document.getElementById('tipo');
+const nombre = document.getElementById('nombre');
+const dueno = document.getElementById('dueno');
+
+console.log(" Mascotas ");
+
+let mascotas =[
+    {
+        tipo: "Gato",
+        nombre: "mancas",
+        dueno: "Esteban"
+    },
+    {
+        tipo: "Gato",
+        nombre: "mancas",
+        dueno: "Esteban"
+    }
+];
+
+function listarMascotas(){
+    const htmlMascotas = mascotas.map((mascota, indice) => `<tr>
+    <th scope="row">${indice}</th>
+    <td>${mascota.tipo}</td>
+    <td>${mascota.nombre}</td>
+    <td>${mascota.dueno}</td>
+    <td>
+        <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-info"> <i class="far fa-edit"> </i></button>
+            <button type="button" class="btn btn-danger"> <i class="far fa-trash-alt"></i></button>
+          </div>  
+    </td>
+  </tr>`).join("");
+  listaMascotas.innerHTML = htmlMascotas;
+}
+listarMascotas();
