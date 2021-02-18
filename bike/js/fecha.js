e5bd2dcbf9564b775dc988625f8657a258837bc0
rhulk <quique1904@gmail.com>
2021-02-18 23:39:46 +0100

    Recuperamos hora y cerramos

--- bike/js/fecha.js
+++ bike/js/fecha.js
@@ -1,6 +1,8 @@
 console.log(' fecha.js');
 const horas = document.getElementById('listaHoras');
 
+
+
 $(document).ready(function(){
 
     $('.datepicker').datepicker({
@@ -22,79 +24,93 @@ $(document).ready(function(){
         const htmlHoras = `
         <div class="row text-center mx-0">
         <div class="col-md-2 col-4 my-1 px-2">
-            <div id="hora" class="cell py-1">9:00AM</div>
+            <div id="900" onclick="getHora(this.id)"  class="cell py-1">9:00AM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">9:30AM</div>
+            <div id="930" onclick="getHora(this.id)" class="cell py-1">9:30AM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">9:45AM</div>
+            <div id="945" onclick="getHora(this.id)" class="cell py-1">9:45AM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">10:00AM</div>
+            <div id="1000" onclick="getHora(this.id)" class="cell py-1">10:00AM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">10:30AM</div>
+            <div id="1030" onclick="getHora(this.id)" class="cell py-1">10:30AM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">10:45AM</div>
-        </div>
-    </div>
-    <div class="row text-center mx-0">
-        <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">11:00AM</div>
-        </div>
-        <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">11:30AM</div>
-        </div>
-        <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">11:45AM</div>
-        </div>
-        <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">12:00PM</div>
-        </div>
-        <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">12:30PM</div>
-        </div>
-        <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">12:45PM</div>
+            <div id="1045" onclick="getHora(this.id)" class="cell py-1">10:45AM</div>
         </div>
     </div>
     <div class="row text-center mx-0">
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">1:00PM</div>
+            <div id="1100" onclick="getHora(this.id)" class="cell py-1">11:00AM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">1:30PM</div>
+            <div id="1130" onclick="getHora(this.id)" class="cell py-1">11:30AM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">1:45PM</div>
+            <div id="1145" onclick="getHora(this.id)" class="cell py-1">11:45AM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">2:00PM</div>
+            <div id="1200" onclick="getHora(this.id)" class="cell py-1">12:00PM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">2:30PM</div>
+            <div id="1230" onclick="getHora(this.id)" class="cell py-1">12:30PM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">2:45PM</div>
+            <div id="1245" onclick="getHora(this.id)" class="cell py-1">12:45PM</div>
         </div>
     </div>
     <div class="row text-center mx-0">
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">3:00PM</div>
+            <div id="100" onclick="getHora(this.id)" class="cell py-1">1:00PM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">3:30PM</div>
+            <div id="130" onclick="getHora(this.id)" class="cell py-1">1:30PM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">4:15PM</div>
+            <div id="145" onclick="getHora(this.id)" class="cell py-1">1:45PM</div>
         </div>
         <div class="col-md-2 col-4 my-1 px-2">
-            <div class="cell py-1">5:00PM</div>
+            <div id="200" onclick="getHora(this.id)" class="cell py-1">2:00PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div id="230" onclick="getHora(this.id)" class="cell py-1">2:30PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div  id="245" onclick="getHora(this.id)" class="cell py-1">2:45PM</div>
+        </div>
+    </div>
+    <div class="row text-center mx-0">
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div  id="300" onclick="getHora(this.id)" class="cell py-1">3:00PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div id="330" onclick="getHora(this.id)" class="cell py-1">3:30PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div id="415" onclick="getHora(this.id)" class="cell py-1">4:15PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div id="500" onclick="getHora(this.id)" class="cell py-1">5:00PM</div>
         </div>
     </div>`;
     horas.innerHTML = htmlHoras;
     
     }
+    function clearHoras(){
+
+        const htmlHoras = ``;
+        horas.innerHTML = htmlHoras;
+        console.log(document.getElementById("listaHoras").className);
+        document.getElementById("listaHoras").className ="";
+        //document.getElementById("listaHoras").classList.add('class');
+    }
+
     loadHoras();
+    function getHora(id){
+        console.log(document.getElementById(id).textContent);
+        clearHoras();
+    }
+    
