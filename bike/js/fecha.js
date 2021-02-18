255bf03ccec84bb27123872be182bc1b42668dfe
kk <quique1904@gmail.com>
2021-02-18 10:57:32 +0100

    reubicación

--- bike/js/fecha.js
+++ bike/js/fecha.js
@@ -1,4 +1,6 @@
 console.log(' fecha.js');
+const horas = document.getElementById('listaHoras');
+
 $(document).ready(function(){
 
     $('.datepicker').datepicker({
@@ -13,3 +15,86 @@ $(document).ready(function(){
     });
     
     });
+
+
+    function loadHoras(){
+
+        const htmlHoras = `
+        <div class="row text-center mx-0">
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">9:00AM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">9:30AM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">9:45AM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">10:00AM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">10:30AM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">10:45AM</div>
+        </div>
+    </div>
+    <div class="row text-center mx-0">
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">11:00AM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">11:30AM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">11:45AM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">12:00PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">12:30PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">12:45PM</div>
+        </div>
+    </div>
+    <div class="row text-center mx-0">
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">1:00PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">1:30PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">1:45PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">2:00PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">2:30PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">2:45PM</div>
+        </div>
+    </div>
+    <div class="row text-center mx-0">
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">3:00PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">3:30PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">4:15PM</div>
+        </div>
+        <div class="col-md-2 col-4 my-1 px-2">
+            <div class="cell py-1">5:00PM</div>
+        </div>
+    </div>`;
+    horas.innerHTML = htmlHoras;
+    
+    }
+    loadHoras();
