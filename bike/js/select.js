b52124b33f1eb37183e4525d746252d2f517dc12
rhulk <quique1904@gmail.com>
2021-02-21 23:44:19 +0100

    contac

--- bike/js/select.js
+++ bike/js/select.js
@@ -7,7 +7,14 @@ const datosTecnicos = document.getElementById("infoTecnica");
 
 let ficha =[
     {
-        cuerpo:"Ficha Tecnica: Specialized diseñó el Turbo Levo"
+        cuerpo:"Ficha Tecnica: Specialized diseñó el Turbo Levo<br>"+
+        "Specialized diseñó el Turbo Levo principalmente como una verdadera moto de pista. <br>"+
+        "En términos de geometría, cinemática y lenguaje de diseño, Specialized utiliza la <br>"+
+        "última bicicleta de pista, la Stumpjumper. Además, la moto ha sido equipada con <br>"+
+        "tecnología líder en el mercado que le da el poder de recorrer más senderos.<br>"+
+        "¿Nuevo motor? Comprobado. ¿Nueva batería? Comprobado. ¿Un marco ultraligero <br>"+
+        "y poderoso? Revisa dos veces.<br>"
+        
     },
     {
         cuerpo: " Ficha Tecnica: Bike 2"
